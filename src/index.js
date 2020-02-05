9b7759255d1a3c3d9006b34c867f5c58ae6d9dda
Will Taylor <willtaylor@Hyperion.lan>
2020-02-04 21:05:07 -0800

    initial outline

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 
 
 console.log('hello world');
+
+// define functions for handling homicide & side-annotation
+
+// suicide & side-annotation
+
+// other granularities
+
+// draw SVG elements, graph titles, axes (fxn w/ dataset input)
+//      mouse-over DoD?
+
+// read in CSV data
+
+// initial call-back renders homicide bubbles, waits 10 seconds,
+// draws next etc. until end
+
+// set up listeners for on-click / on-drag w/ a slider
+
+
